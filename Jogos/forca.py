0ece59df02302d16a40960a56098078c0663c032
Thainara Furforo <thaifurforo@gmail.com>
2022-01-08 19:22:44 -0300

    Ajustando diretórios

--- Jogos/forca.py
+++
@@ -1,9 +0,0 @@
-import jogos
-import forca
-
-def main():
-    print('****************************\nBem vindo ao jogo da Forca!\n****************************')
-    jogos.func_fazer_agora(forca)
-
-if (__name__ == '__main__'):
-    main()
